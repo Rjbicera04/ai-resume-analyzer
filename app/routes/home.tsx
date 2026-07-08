5fb1313724848015a9d9bf4cdecc011ce0f9d76a
Rjbicera04 <rjbicera04@gmail.com>
2026-07-09 02:45:45 +0800

    added puter.ts lib & puter.d.ts types, resume auth page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,9 +13,8 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
   <Navbar />
-
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
