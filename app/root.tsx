a37612f1b154b427b9887cea690f6e8f39d01bac
Rjbicera04 <rjbicera04@gmail.com>
2026-07-13 03:08:08 +0800

    upload Start

--- app/root.tsx
+++ app/root.tsx
@@ -33,20 +33,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
   }, [init]);
 
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <script src="https://js.puter.com/v2/"></script>
-      {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+      <html lang="en">
+          <head>
+              <meta charSet="utf-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <Meta />
+              <Links />
+          </head>
+          <body>
+              <script src="https://js.puter.com/v2/"></script>
+              {children}
+              <ScrollRestoration />
+              <Scripts />
+          </body>
+      </html>
   );
 }
 
